9c612827cf860b240f9ad0a9aaf53650b9364653
Maxence Raballand <dev@maxencerb.com>
2024-07-12 23:01:05 +0200

    chore: init deps

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -5,20 +5,20 @@ import {Test, console} from "forge-std/Test.sol";
 import {Counter} from "../src/Counter.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+  Counter public counter;
 
-    function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
-    }
+  function setUp() public {
+    counter = new Counter();
+    counter.setNumber(0);
+  }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
+  function test_Increment() public {
+    counter.increment();
+    assertEq(counter.number(), 1);
+  }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
+  function testFuzz_SetNumber(uint256 x) public {
+    counter.setNumber(x);
+    assertEq(counter.number(), x);
+  }
 }
